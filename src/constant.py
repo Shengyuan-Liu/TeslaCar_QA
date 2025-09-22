48372e755d2c6061c31cc4044d2f067813f4a935
Shengyuan-Liu <sl5523@ic.ac.uk>
2025-09-22 15:03:59 +0800

    variables and config

---
+++ src/constant.py
@@ -0,0 +1,12 @@
+# Base Direction
+base_dir = "D:/Project/"  # need to be changed
+
+# Data Path
+pdf_path = base_dir + "data/Tesla_Manual.pdf"
+test_doc_path = base_dir + "data/test_docs.txt"
+stopwords_path = base_dir + "data/stopwords.txt"
+image_save_dir = base_dir + "data/saved_images"
+raw_docs_path = base_dir + "data/processed_docs/raw_docs.pkl"
+clean_docs_path = base_dir + "data/processed_docs/clean_docs.pkl"
+split_docs_path = base_dir + "data/processed_docs/split_docs.pkl"
+
